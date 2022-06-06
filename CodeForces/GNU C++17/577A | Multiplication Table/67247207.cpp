7090b6125c478d5fc0257582eeb4811adda56547
Luis Abraham Sanabria Melecio <48113838+magno309@users.noreply.github.com>
2022-06-06 10:44:04 -0500

    Committing files

---
+++ CodeForces/GNU C++17/577A | Multiplication Table/67247207.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+ 
+int main(){
+    int n,x,c=0;
+    scanf("%d%d", &n,&x);
+    for(int i=1;i<=n;i++){
+        if(x % i == 0 && x/i <=n){
+            c++;
+        }
+    }
+    printf("%d", c);
+    return 0;
+}
