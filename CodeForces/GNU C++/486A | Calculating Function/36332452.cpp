bd6bd8bd836923d8786c07baf1c9a9046cfd853e
Luis Abraham Sanabria Melecio <48113838+magno309@users.noreply.github.com>
2022-06-06 10:48:53 -0500

    Committing files

---
+++ CodeForces/GNU C++/486A | Calculating Function/36332452.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+int main(){
+    long long n;
+    scanf("%I64d", &n);
+    if(n%2==0){
+        printf("%I64d", n/2);
+    }
+    else{
+        printf("-%I64d", (n+1)/2);
+    }
+    return 0;
+}
