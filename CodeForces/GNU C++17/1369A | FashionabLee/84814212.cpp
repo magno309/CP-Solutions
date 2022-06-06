b98b6d76e47624db0905db620a3ae04cd82d398e
Luis Abraham Sanabria Melecio <48113838+magno309@users.noreply.github.com>
2022-06-06 10:43:29 -0500

    Committing files

---
+++ CodeForces/GNU C++17/1369A | FashionabLee/84814212.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+ 
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        if(n%2==0 and ((n*(n-3))/2)%2==0)printf("YES\n");
+        else printf("NO\n");
+    }
+    return 0;
+}
