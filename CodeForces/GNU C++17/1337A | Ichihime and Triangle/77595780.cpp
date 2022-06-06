d148eb96ce87b0206574a9d0be4983d0709db23d
Luis Abraham Sanabria Melecio <48113838+magno309@users.noreply.github.com>
2022-06-06 10:43:32 -0500

    Committing files

---
+++ CodeForces/GNU C++17/1337A | Ichihime and Triangle/77595780.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<math.h>
+ 
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0;i<t;i++){
+        int a,b,c,d;
+        bool flag = false;
+        scanf("%d%d%d%d", &a,&b,&c,&d);
+        printf("%d %d %d\n", b,c,c);
+    }
+    return 0;
+}
