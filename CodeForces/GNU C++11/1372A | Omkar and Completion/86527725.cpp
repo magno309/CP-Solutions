4c43d8f593ac97e53c2f7b22022379d6fdf3ad7f
Luis Abraham Sanabria Melecio <48113838+magno309@users.noreply.github.com>
2022-06-06 10:43:21 -0500

    Committing files

---
+++ CodeForces/GNU C++11/1372A | Omkar and Completion/86527725.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+ 
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        for(int i=0;i<n;i++){
+            printf("1 ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
